f8a4a4df3a687daac6d21ec7679d4d37d93d1c28
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-11-05 10:19:39 +0300

    :bug: fixes on the admin page

--- tasks/admin.py
+++ tasks/admin.py
@@ -4,24 +4,30 @@ from tasks.models import Task, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("get_owner_username", "category",
-                    "completed")
+    list_display = ("get_owner_full_name", "category", "completed")
     list_filter = ["completed"]
 
-    def get_owner_username(self, obj):
-        return obj.owner.user.username
-    get_owner_username.short_description = "Owner"
-    get_owner_username.admin_order_field = "owner__user__username"
+    def get_owner_full_name(self, obj):
+        return obj.owner.user.full_name
+
+    get_owner_full_name.short_description = "Owner"
+    get_owner_full_name.admin_order_field = "owner__user__full_name"
 
 
 @admin.register(Task)
 class Taskdmin(admin.ModelAdmin):
-    list_display = ("get_owner_username", "category",
-                    "completed", "due_date",
-                    "important", "created_at")
+    list_display = (
+        "get_owner_full_name",
+        "category",
+        "completed",
+        "due_date",
+        "important",
+        "created_at",
+    )
     list_filter = ("completed", "important")
 
-    def get_owner_username(self, obj):
-        return obj.owner.user.username
-    get_owner_username.short_description = "Owner"
-    get_owner_username.admin_order_field = "owner__user__username"
+    def get_owner_full_name(self, obj):
+        return obj.owner.user.full_name
+
+    get_owner_full_name.short_description = "Owner"
+    get_owner_full_name.admin_order_field = "owner__user__full_name"
