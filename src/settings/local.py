60ba4a19ba1d01de3caf644c87e8bdd840a0b357
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-11-08 12:16:13 +0300

    :sparkles: fixes on access token and refresh_token life time

--- src/settings/local.py
+++ src/settings/local.py
@@ -52,10 +52,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    # "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
-    # "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=3),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=120),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
