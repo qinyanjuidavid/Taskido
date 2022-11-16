e69b4042b2671282cbbfc74ece06f39514f7de64
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-11-16 17:18:17 +0300

    :bug: fixes

--- src/settings/local.py
+++ src/settings/local.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=120),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
