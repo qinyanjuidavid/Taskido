fe500a18551cc191ab2425dde48caac4011e0765
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-11-21 14:04:01 +0300

    :ok_hand: large pagination

--- tasks/paginations.py
+++ tasks/paginations.py
@@ -2,7 +2,14 @@ from rest_framework import pagination
 
 
 class StandardResultsSetPagination(pagination.PageNumberPagination):
-    page_size = 8
+    page_size = 6
+    page_query_param = "page"
+    page_size_query_param = "page_size"
+    max_page_size = 1000
+
+
+class LargeResultsSetPagination(pagination.PageNumberPagination):
+    page_size = 100
     page_query_param = "page"
     page_size_query_param = "page_size"
     max_page_size = 1000
